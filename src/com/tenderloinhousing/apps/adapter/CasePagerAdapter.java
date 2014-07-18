72e8304e6cc37005ee273999baaa24a7dfd65d0c
Cindy Tianqing Li <cindyltq@hotmail.com>
2014-07-18 16:44:15 -0700

    Implemented "Search Case By ID" fragment

--- src/com/tenderloinhousing/apps/adapter/CasePagerAdapter.java
+++ src/com/tenderloinhousing/apps/adapter/CasePagerAdapter.java
@@ -7,11 +7,12 @@ import android.support.v4.app.FragmentPagerAdapter;
 import com.parse.ParseUser;
 import com.tenderloinhousing.apps.fragment.AllCaseListFragment;
 import com.tenderloinhousing.apps.fragment.MyCaseListFragment;
+import com.tenderloinhousing.apps.fragment.SearchCaseFragment;
 import com.tenderloinhousing.apps.model.User;
 
 public class CasePagerAdapter extends FragmentPagerAdapter
 {
-    private static final String[] CONTENT = new String[] { "All Cases", "My Cases" };
+    private static final String[] CONTENT = new String[] { "All Cases", "My Cases", "Search by #" };
 
     public CasePagerAdapter(FragmentManager fragmentManager)
     {
@@ -26,8 +27,10 @@ public class CasePagerAdapter extends FragmentPagerAdapter
 	{
 	case 0: // Fragment # 0
 	    return new AllCaseListFragment();
-	case 1: // Fragment # 0
+	case 1: // Fragment # 1
 	    return MyCaseListFragment.newInstance((User) ParseUser.getCurrentUser());
+	case 2: // Fragment # 2
+	    return new SearchCaseFragment();
 	default:
 	    return null;
 	}
@@ -36,7 +39,7 @@ public class CasePagerAdapter extends FragmentPagerAdapter
     @Override
     public CharSequence getPageTitle(int position)
     {
-	return CONTENT[position % CONTENT.length].toUpperCase();
+	return CONTENT[position % CONTENT.length];
     }
 
     @Override
---
+++ src/com/tenderloinhousing/apps/fragment/SearchCaseFragment.java
@@ -0,0 +1,111 @@
+package com.tenderloinhousing.apps.fragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+import android.widget.Toast;
+
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.tenderloinhousing.apps.R;
+import com.tenderloinhousing.apps.constant.IConstants;
+import com.tenderloinhousing.apps.dao.ParseDAO;
+import com.tenderloinhousing.apps.model.Case;
+
+public class SearchCaseFragment extends Fragment implements IConstants
+{
+    EditText etCaseId;
+    String caseId;
+
+    @Override
+    public void onAttach(Activity activity)
+    {
+	super.onAttach(activity);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+	super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState)
+    {
+	View view = inflater.inflate(R.layout.fragment_search_case, parent, false);
+	etCaseId = (EditText) view.findViewById(R.id.etCaseId);
+	etCaseId.setOnEditorActionListener(getOnEditorActionListener());
+	return view;
+    }
+
+    private OnEditorActionListener getOnEditorActionListener()
+    {
+	return new OnEditorActionListener()
+	{
+	    @Override
+	    public boolean onEditorAction(TextView view, int actionId, KeyEvent event)
+	    {
+		int result = actionId & EditorInfo.IME_MASK_ACTION;
+		switch (result)
+    		{
+        		case EditorInfo.IME_ACTION_DONE:
+        		    caseId = etCaseId.getText().toString();
+        		    searchAndDisplay(caseId);
+        		    break;
+        		default:
+        		    caseId = etCaseId.getText().toString();
+        		    searchAndDisplay(caseId);
+        		    break;
+		}
+		return false;
+	    }
+	};
+    }
+
+    public void searchAndDisplay(String caseId)
+    {
+	ParseDAO.getCaseById(caseId, new GetCallback<Case>()
+	{
+	    @Override
+	    public void done(Case foundCase, ParseException e)
+	    {
+		if (e == null)
+		{
+		    if (foundCase != null)
+		    {
+			Log.d(DEBUG, " foundCase " + foundCase.getBuilding().getAddress());
+			loadCaseDetailFragment(foundCase);
+		    }
+		}
+		else
+		{
+		    Toast.makeText(getActivity(), "No case with that id", Toast.LENGTH_LONG).show();
+		    Log.d(ERROR, "Error: " + e.getMessage());
+		}
+
+	    }
+	});
+    }
+
+    public void loadCaseDetailFragment(Case foundCase)
+    {
+	Bundle bundle = new Bundle();
+	bundle.putSerializable(CASE_KEY, foundCase);
+	CaseDetailsFragment detailsFragment = CaseDetailsFragment.newInstance(bundle);
+
+	getActivity().getSupportFragmentManager()
+		.beginTransaction()
+		.add(R.id.flDetailContainer, detailsFragment)
+		.commit();
+    }
+
+}
